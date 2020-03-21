b6e98d1bd9cedb97e9f51574eb8af59cd8fabf20
Stefani334 <sjovanoska00@gmail.com>
2020-03-22 00:57:08 +0100

    point class finished

--- point.java
+++ point.java
@@ -1,1 +1,19 @@
 String s =  "red";
+Point(String s){
+this.s = s;
+}
+
+public String getS(){
+return s;
+}
+
+public String setS(String s){
+this.s = s;
+}
+
+void move(char xDirection, char yDirection){
+if(xDirection > 0 && yDirection > 0){
+
+}
+void draw(){
+}
