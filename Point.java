a6b41d3431d5bca1fea4ed6a970f70fa883444b9
jovanoskas <sjovanoska@outlook.com>
2020-03-23 20:48:39 +0100

    color variable added

---
+++ Point.java
@@ -0,0 +1,20 @@
+
+class Point {
+        String id
+        double x,y;
+
+        //TODO add new variable
+        String s = "red";
+        //TODO constructor
+
+        //TODO setters and getters
+
+        public void move (char xDirection, char yDirection) {
+
+                //TODO
+        }
+
+        public void draw () {
+                //TODO
+        }
+}
